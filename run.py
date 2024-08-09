73b772b58c5cb839f8abcd4c83c46557e46d7c40
kyle-lucke <luck0692@vandals.uidaho.edu>
2024-08-09 12:35:51 -0600

    change setup to only save best model base on validation RMSE, add seeding for reproducability, move call to scheduler to end of epoch, save args to ouput directory.

--- run.py
+++ run.py
@@ -8,53 +8,89 @@ from torch.optim.lr_scheduler import MultiStepLR
 import torch.nn as nn
 from torch.autograd import Variable
 import copy
+import json
 import time
+import random
 from pathlib import Path
-from logger import Logger
 from math import sqrt
 import numpy as np
 import os
 
+def save_args(args_dict, save_fname):
+    json.dump(args_dict, open(save_fname, 'w'))
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
 parser = argparse.ArgumentParser(description='RecoEncoder')
 parser.add_argument('--lr', type=float, default=0.00001, metavar='N',
                     help='learning rate')
+
 parser.add_argument('--weight_decay', type=float, default=0.0, metavar='N',
                     help='L2 weight decay')
+
 parser.add_argument('--drop_prob', type=float, default=0.0, metavar='N',
                     help='dropout drop probability')
+
 parser.add_argument('--noise_prob', type=float, default=0.0, metavar='N',
                     help='noise probability')
+
 parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                     help='global batch size')
+
 parser.add_argument('--summary_frequency', type=int, default=100, metavar='N',
                     help='how often to save summaries')
+
 parser.add_argument('--aug_step', type=int, default=-1, metavar='N',
                     help='do data augmentation every X step')
+
 parser.add_argument('--constrained', action='store_true',
                     help='constrained autoencoder')
+
 parser.add_argument('--skip_last_layer_nl', action='store_true',
                     help='if present, decoder\'s last layer will not apply non-linearity function')
+
 parser.add_argument('--num_epochs', type=int, default=50, metavar='N',
                     help='maximum number of epochs')
+
 parser.add_argument('--save_every', type=int, default=3, metavar='N',
                     help='save every N number of epochs')
+
 parser.add_argument('--optimizer', type=str, default="momentum", metavar='N',
                     help='optimizer kind: adam, momentum, adagrad or rmsprop')
+
 parser.add_argument('--hidden_layers', type=str, default="1024,512,512,128", metavar='N',
                     help='hidden layer sizes, comma-separated')
+
 parser.add_argument('--gpu_ids', type=str, default="0", metavar='N',
                     help='comma-separated gpu ids to use for data parallel training')
+
 parser.add_argument('--path_to_train_data', type=str, default="", metavar='N',
                     help='Path to training data')
+
 parser.add_argument('--path_to_eval_data', type=str, default="", metavar='N',
                     help='Path to evaluation data')
+
 parser.add_argument('--non_linearity_type', type=str, default="selu", metavar='N',
                     help='type of the non-linearity used in activations')
+
 parser.add_argument('--logdir', type=str, default="logs", metavar='N',
                     help='where to save model and write logs')
 
+parser.add_argument('--seed', type=int, default=0, help='Seed for RNGs')
+
 args = parser.parse_args()
 print(args)
+
+set_seed(args.seed)
 
 use_gpu = torch.cuda.is_available() # global flag
 if use_gpu:
@@ -75,35 +111,8 @@ def do_eval(encoder, evaluation_data_layer):
     denom += num_ratings.item()
   return sqrt(total_epoch_loss / denom)
 
-def log_var_and_grad_summaries(logger, layers, global_step, prefix, log_histograms=False):
-  """
-  Logs variable and grad stats for layer. Transfers data from GPU to CPU automatically
-  :param logger: TB logger
-  :param layers: param list
-  :param global_step: global step for TB
-  :param prefix: name prefix
-  :param log_histograms: (default: False) whether or not log histograms
-  :return:
-  """
-  for ind, w in enumerate(layers):
-    # Variables
-    w_var = w.data.cpu().numpy()
-    logger.scalar_summary("Variables/FrobNorm/{}_{}".format(prefix, ind), np.linalg.norm(w_var),
-                          global_step)
-    if log_histograms:
-      logger.histo_summary(tag="Variables/{}_{}".format(prefix, ind), values=w.data.cpu().numpy(),
-                           step=global_step)
-
-    # Gradients
-    w_grad = w.grad.data.cpu().numpy()
-    logger.scalar_summary("Gradients/FrobNorm/{}_{}".format(prefix, ind), np.linalg.norm(w_grad),
-                          global_step)
-    if log_histograms:
-      logger.histo_summary(tag="Gradients/{}_{}".format(prefix, ind), values=w.grad.data.cpu().numpy(),
-                         step=global_step)
 
 def main():
-  logger = Logger(args.logdir)
   params = dict()
   params['batch_size'] = args.batch_size
   params['data_dir'] =  args.path_to_train_data
@@ -136,6 +145,8 @@ def main():
     print("Loading model from: {}".format(model_checkpoint))
     rencoder.load_state_dict(torch.load(model_checkpoint))
 
+  save_args(vars(args), os.path.join(args.logdir, 'exp_args.json'))
+
   print('######################################################')
   print('######################################################')
   print('############# AutoEncoder Model: #####################')
@@ -143,6 +154,8 @@ def main():
   print('######################################################')
   print('######################################################')
 
+  best_val_rmse = float('inf')
+  
   gpu_ids = [int(g) for g in args.gpu_ids.split(',')]
   print('Using GPUs: {}'.format(gpu_ids))
   if len(gpu_ids)>1:
@@ -184,8 +197,6 @@ def main():
     rencoder.train()
     total_epoch_loss = 0.0
     denom = 0.0
-    if args.optimizer == "momentum":
-      scheduler.step()
     for i, mb in enumerate(data_layer.iterate_one_epoch()):
       inputs = Variable(mb.cuda().to_dense() if use_gpu else mb.to_dense())
       optimizer.zero_grad()
@@ -199,19 +210,14 @@ def main():
       t_loss_denom += 1
 
       if i % args.summary_frequency == 0:
-        print('[%d, %5d] RMSE: %.7f' % (epoch, i, sqrt(t_loss / t_loss_denom)))
-        logger.scalar_summary("Training_RMSE", sqrt(t_loss/t_loss_denom), global_step)
+        print('[%d, %5d] RMSE: %.7f' % (epoch, i, sqrt(t_loss / t_loss_denom)), flush=True)
+
         t_loss = 0
         t_loss_denom = 0.0
-        log_var_and_grad_summaries(logger, rencoder.encode_w, global_step, "Encode_W")
-        log_var_and_grad_summaries(logger, rencoder.encode_b, global_step, "Encode_b")
-        if not rencoder.is_constrained:
-          log_var_and_grad_summaries(logger, rencoder.decode_w, global_step, "Decode_W")
-        log_var_and_grad_summaries(logger, rencoder.decode_b, global_step, "Decode_b")
 
       total_epoch_loss += loss.item()
       denom += 1
-
+      
       #if args.aug_step > 0 and i % args.aug_step == 0 and i > 0:
       if args.aug_step > 0:
         # Magic data augmentation trick happen here
@@ -226,18 +232,28 @@ def main():
           loss.backward()
           optimizer.step()
 
+    if args.optimizer == "momentum":
+        scheduler.step()
+  
     e_end_time = time.time()
     print('Total epoch {} finished in {} seconds with TRAINING RMSE loss: {}'
           .format(epoch, e_end_time - e_start_time, sqrt(total_epoch_loss/denom)))
-    logger.scalar_summary("Training_RMSE_per_epoch", sqrt(total_epoch_loss/denom), epoch)
-    logger.scalar_summary("Epoch_time", e_end_time - e_start_time, epoch)
+
     if epoch % args.save_every == 0 or epoch == args.num_epochs - 1:
       eval_loss = do_eval(rencoder, eval_data_layer)
-      print('Epoch {} EVALUATION LOSS: {}'.format(epoch, eval_loss))
-      logger.scalar_summary("EVALUATION_RMSE", eval_loss, epoch)
-      print("Saving model to {}".format(model_checkpoint + ".epoch_"+str(epoch)))
-      torch.save(rencoder.state_dict(), model_checkpoint + ".epoch_"+str(epoch))
 
+      print('Epoch {} EVALUATION LOSS: {}'.format(epoch, eval_loss))
+      
+      if eval_loss < best_val_rmse:
+
+          out_fname = model_checkpoint + ".best"
+          
+          print(f'Validation loss improved from {best_val_rmse} to {eval_loss}, saving model. ')
+          print(f"Saving model to {out_fname}")
+          torch.save(rencoder.state_dict(), out_fname)
+
+          best_val_rmse = eval_loss
+          
   print("Saving model to {}".format(model_checkpoint + ".last"))
   torch.save(rencoder.state_dict(), model_checkpoint + ".last")
 
